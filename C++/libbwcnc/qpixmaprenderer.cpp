40bc0ab634176076f4a0abbe8ee7b48aba323d6a
Christiana Johnson <anajilly@gmail.com>
2018-02-27 02:02:07 -0800

    allows qpainter to be owned by some other object

--- C++/libbwcnc/qpixmaprenderer.cpp
+++ C++/libbwcnc/qpixmaprenderer.cpp
@@ -9,8 +9,8 @@ void BWCNC::PixmapRenderer::drawline( const BWCNC::Command * cmd, const BWCNC::C
     if( ! render_negative_z && (cmd->begin[2] < 0 || cmd->end[2] < 0) ) return;
 
     pen.setColor( QColor(cmd->clr.to_rgb24()) );
-    p.setPen( pen );
-    p.drawLine( cmd->begin[0], cmd->begin[1], cmd->end[0], cmd->end[1] );
+    p->setPen( pen );
+    p->drawLine( cmd->begin[0], cmd->begin[1], cmd->end[0], cmd->end[1] );
 
     if( debug_countdown > 0 )
     {
@@ -28,8 +28,8 @@ void BWCNC::PixmapRenderer::draw_dot( const BWCNC::Command * cmd, const BWCNC::C
     if( ! render_negative_z && cmd->begin[2] < 0 ) return;
 
     pen.setColor( QColor(clr.to_rgb24()) );
-    p.setPen( pen );
-    p.drawEllipse( QPoint(cmd->begin[0], cmd->begin[1]), 1, 1 );
+    p->setPen( pen );
+    p->drawEllipse( QPoint(cmd->begin[0], cmd->begin[1]), 1, 1 );
 
     if( debug_countdown > 0 )
     {
@@ -48,8 +48,8 @@ void BWCNC::PixmapRenderer::print_start( const BWCNC::Boundingbox & )
 
     if( bool(backgd_color) )
         im->fill( QColor(backgd_color.to_rgb24()) );
-    p.begin(im);
-    p.setRenderHint( QPainter::Antialiasing );
+    p->begin(im);
+    p->setRenderHint( QPainter::Antialiasing );
     pen.setWidth(1);
 
 }
--- C++/libbwcnc/qpixmaprenderer.h
+++ C++/libbwcnc/qpixmaprenderer.h
@@ -13,8 +13,9 @@ class PixmapRenderer : public BWCNC::GraphicalRenderer
 {
 public:
     PixmapRenderer() {}
-    PixmapRenderer( QImage * img ) : im(img) {}
-    virtual ~PixmapRenderer() {}
+    PixmapRenderer( QImage * img ) : im(img) { p = new QPainter; painter_is_owned = true; }
+    PixmapRenderer( QImage * img, QPainter * pntr ) : im(img), p(pntr) {}
+    virtual ~PixmapRenderer() { if(painter_is_owned) delete p; }
 
     void set_image( QImage * img ) { im = img; }
     int debug_countdown = 0;
@@ -23,8 +24,11 @@ public:
 
 protected:
     QImage * im = nullptr;
-    QPainter p;
+    QPainter * p = nullptr;
     QPen pen; // lineto_pen; moveto_pen;
+
+private:
+    bool painter_is_owned = false;
 
 protected:
     virtual void drawline( const BWCNC::Command * cmd, const BWCNC::Color & clr ) ;
@@ -32,7 +36,7 @@ protected:
   //virtual void drawarc(  const BWCNC::Command * cmd, const BWCNC::Color & clr ) ;
 
     virtual void print_start( const BWCNC::Boundingbox & );
-    virtual void print_end() { p.end(); }
+    virtual void print_end() { p->end(); }
 };
 
 };
